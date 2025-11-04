43a86812aeae33396710d1e681271d56fce27a7f
AgusCuculich <cuculichagustina@gmail.com>
2025-11-04 16:10:56 -0300

    Base del proyecto

---
+++ microservicio-user/src/main/java/controller/UserController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+import service.UserService;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService service;
+    
+}
---
+++ microservicio-user/src/main/java/entity/User.java
@@ -0,0 +1,17 @@
+package entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private String surname;
+    @Column
+    private Long mobile;
+}
