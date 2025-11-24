4bde60170a1b006ccf0d6898a0bc1766fa7525d0
JulioTudai <Juliotwete@hotmail.com>
2025-11-24 10:27:25 -0300

    Permisos en microservicioTransporte terminados y testeados.

--- microservicio-transporte/src/main/java/com/controllers/MonopatinController.java
+++ microservicio-transporte/src/main/java/com/controllers/MonopatinController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import com.services.MonopatinService;
 
@@ -25,19 +26,23 @@ public class MonopatinController {
     private final MonopatinService monopatinService;
 
     @GetMapping("/debug")
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
     public List<Monopatin> debug() {return monopatinService.getAllMonopatinesDebug();}
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMINISTRADOR', 'MANTENIMIENTO', 'USUARIO')")
     public MonopatinDTO  getMonopatinById(@PathVariable("id") long id){
         return monopatinService.getMonopatinById(id);
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ADMINISTRADOR', 'MANTENIMIENTO')")
     public List<MonopatinDTO> getAllMonopatines(){
         return monopatinService.getAllMonopatines();
     }
 
     @GetMapping("/reporte")
+    @PreAuthorize("hasAnyRole('ADMINISTRADOR', 'MANTENIMIENTO')")
     public List<ReporteMonopatinesDTO> obtenerReporteMonopatines(
             @RequestParam("inicio") @DateTimeFormat(pattern = "yyyy-MM-dd") Date inicio,
             @RequestParam("fin") @DateTimeFormat(pattern = "yyyy-MM-dd") Date fin,
@@ -47,26 +52,31 @@ public class MonopatinController {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
     public void saveMonopatin(@RequestBody Monopatin monopatin){
         monopatinService.saveMonopatin(monopatin);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
     public void deleteMonopatinById(@PathVariable("id")long id){
         monopatinService.deleteMonopatin(id);
     }
 
     @PutMapping("/{id}/parada/{parada_id}")
+    @PreAuthorize("hasAnyRole('ADMINISTRADOR', 'MANTENIMIENTO')")
     public MonopatinDTO ubicarMonopatinEnParada(@PathVariable("id")long id, @PathVariable("parada_id")long parada_id){
         return monopatinService.ubicarMonopatinEnParada(id,parada_id);
     }
 
     @PutMapping("/{id}/estado")
+    @PreAuthorize("hasAnyRole('ADMINISTRADOR', 'MANTENIMIENTO')")
     public void actualizarEstado(@PathVariable("id") Long id, @RequestParam("estado") EstadoMonopatin estado) {
         monopatinService.actualizarEstado(id, estado);
     }
 
     @GetMapping("/stats")
+    @PreAuthorize("hasAnyRole('ADMINISTRADOR', 'MANTENIMIENTO')")
     public List<MonopatinDTO> getMonopatinesConMasViajes(
             @RequestParam("anio") int anio,
             @RequestParam("viajes") int viajes) {
@@ -74,6 +84,7 @@ public class MonopatinController {
     }
 
     @GetMapping("/nearby")
+    @PreAuthorize("hasAnyRole('ADMINISTRADOR', 'USUARIO')")
     public List<MonopatinDTO> getAllNearbyScooters(
             @RequestParam("latitud") double latitud,
             @RequestParam("longitud") double longitud,
--- microservicio-transporte/src/main/java/com/controllers/ParadaController.java
+++ microservicio-transporte/src/main/java/com/controllers/ParadaController.java
@@ -4,6 +4,7 @@ import com.dtos.ParadaDTO;
 import com.entities.Parada;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import com.services.ParadaService;
 
@@ -17,21 +18,25 @@ public class ParadaController {
     private final ParadaService paradaService;
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMINISTRADOR', 'MANTENIMIENTO', 'USUARIO')")
     public ParadaDTO getParada(@PathVariable("id") Long id){
         return paradaService.getParada(id);
     }
 
     @GetMapping("/")
+    @PreAuthorize("hasAnyRole('ADMINISTRADOR', 'MANTENIMIENTO', 'USUARIO')")
     public List<ParadaDTO> getParadas(){
         return paradaService.getParadas();
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
     public void saveParada(@RequestBody Parada parada){
         paradaService.saveParada(parada);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMINISTRADOR')")
     public void deleteParada(@PathVariable("id") Long id){
         paradaService.deleteParada(id);
     }
